5c307e737aedb6d6fd2a37f5f62b581deffc46b1
hae-on <solbi2004@naver.com>
2022-02-22 12:01:54 +0900

    mock test

---
+++ UNIT-BASIC/src/mock/product/test/product_service.test.js
@@ -0,0 +1,15 @@
+const ProductService = require("../product_service.js");
+const StubProductClient = require("./stub_product_client.js");
+
+describe("ProductService - Stub", () => {
+  let productService;
+
+  beforeEach(() => {
+    productService = new ProductService(new StubProductClient());
+  });
+
+  it("should filter out only available items", async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(items).toEqual([{ item: "🥛", available: true }]);
+  });
+});
