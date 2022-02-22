bc5d41984454c0aaba27652c242aec97ee0ea060
hae-on <solbi2004@naver.com>
2022-02-22 11:04:08 +0900

    watch mode test

---
+++ UNIT-BASIC/src/mock/check/test/check.test.js
@@ -0,0 +1,16 @@
+const check = require("../check.js");
+
+describe("check", () => {
+  let onSuccess;
+  let onFail;
+
+  beforeEach(() => {
+    onSuccess = jest.fn();
+    onFail = jest.fn();
+  });
+
+  it("should call onSuccess when predicate is true", () => {
+    check(() => true, onSuccess, onFail);
+    expect(onSuccess.mock.calls.length).toBe(1);
+  });
+});
